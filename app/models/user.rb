fe03b98e0affbf544379a9f663ab30305ce51b7d
koziscool <kozjob@yahoo.com>
2016-02-16 12:08:52 -0600

    auth seems to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ActiveRecord::Base
 
- has_secure_password
+  has_secure_password
 
   validates :password, :length => { :in => 5..35 }, :allow_nil => true
+
+  def generate_token
+    begin
+      self[:auth_token] = SecureRandom.urlsafe_base64
+    end while User.exists?(:auth_token => self[:auth_token])
+  end
+
+  def regenerate_auth_token
+    self.auth_token = nil
+    generate_token
+    save!
+  end
+
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212000414) do
+ActiveRecord::Schema.define(version: 20160216180035) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name",       null: false
@@ -83,8 +83,10 @@ ActiveRecord::Schema.define(version: 20160212000414) do
     t.string   "email",      null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "auth_token"
   end
 
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["first_name"], name: "index_users_on_first_name"
   add_index "users", ["last_name"], name: "index_users_on_last_name"
